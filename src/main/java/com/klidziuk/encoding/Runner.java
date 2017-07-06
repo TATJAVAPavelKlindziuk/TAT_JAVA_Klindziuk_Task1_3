bb21e1e1e67ed7ccf6751dd678a6c74104ef4572
Pavel Klindziuk <pavel_klindziuk@epam.com>
2017-07-06 18:33:06 +0300

    upload for check

--- src/main/java/com/klidziuk/encoding/Runner.java
+++
@@ -1,9 +0,0 @@
-package com.klidziuk.encoding;
-
-import com.klidziuk.encoding.fileoperations.CharsetFileReader;
-
-public class Runner {
-	public static void main(String[] args) {
-		new CharsetFileReader().readFilesWithDifferentCharsets();
-	}
-}
